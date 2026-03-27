f4add3b3da528958556d0dd97d8927dd2863d142
Maurizio Ungaro <maureeungaro@gmail.com>
2026-03-27 16:44:41 -0400

    added fairshare table loading

--- web_portal/fairshare.php
+++ web_portal/fairshare.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once __DIR__ . '/head.php'; ?>
-<body>
+<body onload="fairshareToTable();">
 <?php require_once __DIR__ . '/header.php'; ?>
 
 <main class="page-section">
