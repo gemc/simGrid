47ec1d9a3d29e136bdeab330df52df53cafe93ae
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2026-09-14 10:40:21 -0400

    reverting coatjava to 14.1.2

--- web_portal/main.js
+++ web_portal/main.js
@@ -544,9 +544,9 @@ function softwareVersionSelected() {
 			}
 
 			if (isDevelPortal) {
-				const devVersion = "gemc/dev coatjava/14.2.0";
+				const devVersion = "gemc/dev coatjava/14.1.2";
 				text += `<option value="${devVersion}">${devVersion}</option>`;
-				const mesonVersion = "gemc/meson coatjava/14.2.0";
+				const mesonVersion = "gemc/meson coatjava/14.1.2";
 				text += `<option value="${devVersion}">${devVersion}</option>`;
 			}
 
